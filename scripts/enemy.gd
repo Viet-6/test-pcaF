aa43a2fff53b8ab3625c11f11fa86b0e94db3a89
Viet-6 <vlehoangquoc@gmail.com>
2026-01-14 11:27:54 +0000

    fix projectile #22

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,16 +6,6 @@ var current_health: int
 func _ready():
 	current_health = max_health
 	add_to_group("enemies")
-	
-	# Force create collision shape to ensure it exists (Scene file paranoia)
-	var shape = RectangleShape2D.new()
-	shape.size = Vector2(100, 200)
-	
-	var col = CollisionShape2D.new()
-	col.shape = shape
-	col.debug_color = Color(1, 0, 0, 0.5)
-	add_child(col)
-	print("Enemy Initialized with Procedural Collision Shape")
 
 func _physics_process(delta):
 	# Add gravity so it sits on floor properly
