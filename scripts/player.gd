faf25437dd93ccbfa530348330b276869383d4fe
Viet-6 <vlehoangquoc@gmail.com>
2026-01-14 10:07:27 +0000

    fix combat issue #4

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,18 +7,20 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var sprite = $Sprite2D
 
+func _ready():
+	add_to_group("player")
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 		# Flip sprite based on direction
@@ -27,7 +29,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	# Handle Shooting
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_action_pressed("fire"):
 		# Assuming a child node named "Weapon" exists
 		if has_node("Weapon"):
 			$Weapon.shoot()
